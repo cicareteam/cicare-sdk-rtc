a847756bda5b1fac83f6546d8450dd9b044a4f44
annaselh <annas.elh@gmail.com>
2026-01-28 21:37:08 +0700

    rc5

--- sdkcall/build.gradle.kts
+++ sdkcall/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 android {
-    namespace = "cicare-sdk-rtc"
-    version = "1.2.1-rc.3"
+    namespace = "cicare-sdkcall"
+    version = "1.2.1-rc.5"
     compileSdk = 35
 
     defaultConfig {
@@ -79,7 +79,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.cicareteam"
                 artifactId = "cicare-sdk-rtc"
-                version = "1.2.1-rc.4"
+                version = "1.2.1-rc.5"
                 from(components["release"])
             }
         }
